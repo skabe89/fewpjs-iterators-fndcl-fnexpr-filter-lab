44227f88400a11c5d1ff72cf57c77a6166fa6f0c
Sean <skabe89@gmail.com>
2021-02-22 22:57:51 -0500

    commit tree 4988edd10a87ab6c4ed136ce28d1a99eefd17820

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+  return drivers.filter(d => d.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(d => d[0] == string[0])
+}
+
+function matchName(drivers, string){
+  return drivers.filter( d => d.name == string)
+}
